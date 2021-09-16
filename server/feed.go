054ee66994f2caa46b2162f46fcc0b4c478caddf
Vineet <vineet070193@gmail.com>
2021-09-16 23:39:02 +0530

    Implementing feed server

--- server/feed.go
+++ server/feed.go
@@ -25,3 +25,22 @@ func (f *FeedService) AddFeed(ctx context.Context, r *feedprotos.AddFeedRequest)
 		},
 	}, nil
 }
+
+func (f *FeedService) ListFeed(ctx context.Context, r *feedprotos.ListFeedRequest) (*feedprotos.ListFeedResponse, error) {
+	f.log.Info("Handle AddFeed", "feed_id", r.FeedID)
+
+	return &feedprotos.ListFeedResponse{
+		Status: &feedprotos.Status{
+			Error:  nil,
+			Status: true,
+		},
+		FeedList: []*feedprotos.Feed{
+			{
+				FeedID:    "FeedID1",
+				Snippet:   "Snippet1",
+				Content:   "Content",
+				Timestamp: 123456,
+			},
+		},
+	}, nil
+}
